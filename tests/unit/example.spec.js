d13ba8e84bc7b4dc67f7b5a70e87039d2377cbab
Roberto-Irigoyen-ou <roberto.irigoyen@oubita.com>
2024-04-24 13:29:21 +0200

    Test prueba

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,13 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+describe('Example component', () => {
+  test('Debe ser mayor a 10', () => {
+    //Arreglar
+    let value = 9
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+    //Estimulo
+
+    value = value + 2
+
+    //Observar
+    expect(value).toBeGreaterThan(10)
   })
 })
